5c42f775a07cc686a2a0fc280a4fe3d93dd70f8e
tposejank <81495861+tposejank@users.noreply.github.com>
2025-05-31 09:16:55 -0400

    yeah

--- festivalinfobot.py
+++ festivalinfobot.py
@@ -193,7 +193,6 @@ class FestivalInfoBot(commands.AutoShardedBot):
         self.path_handler = PathCommandHandler()
         self.history_handler = HistoryHandler()
         self.check_handler = LoopCheckHandler(self)
-        self.graph_handler = GraphCommandsHandler()
         self.oauth_manager = OAuthManager(self, constants.EPIC_DEVICE_ID, constants.EPIC_ACCOUNT_ID, constants.EPIC_DEVICE_SECRET)
         self.pro_vocals_handler = ProVocalsHandler(self)
 
@@ -604,50 +603,6 @@ class FestivalInfoBot(commands.AutoShardedBot):
                 view = constants.PaginatorView(embeds, interaction.user.id)
                 await interaction.response.send_message(embed=view.get_embed(), view=view)
                 view.message = await interaction.original_response()
-
-        graph_group = app_commands.Group(name="graph", description="Graph Command Group.", allowed_contexts=discord.app_commands.AppCommandContext(guild=True, dm_channel=True, private_channel=True), allowed_installs=discord.app_commands.AppInstallationType(guild=True, user=True))
-
-        graph_notes_group = app_commands.Group(name="counts", description="Graph the note and lift counts for a specific song.", parent=graph_group)
-        @graph_notes_group.command(name="all", description="Graph the note counts for a specific song.")
-        @app_commands.describe(song = "A search query: an artist, song name, or shortname.")
-        async def graph_note_counts_command(interaction: discord.Interaction, song:str):
-            if not self.DECRYPTION_ALLOWED:
-                await interaction.response.send_message(content="This command is not enabled in this bot.", ephemeral=True)
-                return
-            await self.graph_handler.handle_pdi_interaction(interaction=interaction, song=song)
-
-        @graph_notes_group.command(name="lifts", description="Graph the lift counts for a specific song.")
-        @app_commands.describe(song = "A search query: an artist, song name, or shortname.")
-        async def graph_note_counts_command(interaction: discord.Interaction, song:str):
-            if not self.DECRYPTION_ALLOWED:
-                await interaction.response.send_message(content="This command is not enabled in this bot.", ephemeral=True)
-                return
-            
-            await self.graph_handler.handle_lift_interaction(interaction=interaction, song=song)
-
-        @graph_group.command(name="nps", description="Graph the NPS (Notes per second) for a specific song, instrument, and difficulty.")
-        @app_commands.describe(song = "A search query: an artist, song name, or shortname.")
-        @app_commands.describe(instrument = "The instrument to view the NPS of.")
-        @app_commands.describe(difficulty = "The difficulty to view the NPS for.")
-        async def graph_nps_command(interaction: discord.Interaction, song:str, instrument : constants.Instruments, difficulty : constants.Difficulties = constants.Difficulties.Expert):
-            if not self.DECRYPTION_ALLOWED:
-                await interaction.response.send_message(content="This command is not enabled in this bot.", ephemeral=True)
-                return
-            
-            await self.graph_handler.handle_nps_interaction(interaction=interaction, song=song, instrument=instrument, difficulty=difficulty)
-
-        @graph_group.command(name="lanes", description="Graph the number of notes for each lane in a specific song, instrument, and difficulty.")
-        @app_commands.describe(song = "A search query: an artist, song name, or shortname.")
-        @app_commands.describe(instrument = "The instrument to view the #notes of.")
-        @app_commands.describe(difficulty = "The difficulty to view the #notes for.")
-        async def graph_lanes_command(interaction: discord.Interaction, song:str, instrument : constants.Instruments, difficulty : constants.Difficulties = constants.Difficulties.Expert):
-            if not self.DECRYPTION_ALLOWED:
-                await interaction.response.send_message(content="This command is not enabled in this bot.", ephemeral=True)
-                return
-            
-            await self.graph_handler.handle_lanes_interaction(interaction=interaction, song=song, instrument=instrument, difficulty=difficulty)
-
-        self.tree.add_command(graph_group)
 
         @self.tree.command(name="jswiki", description="View the Jam Track wiki.")
         @app_commands.allowed_installs(guilds=True, users=True)
