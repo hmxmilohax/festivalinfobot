a99f025701310b7dfd55f0501dfcd82beb2dbac7
tposejank <81495861+tposejank@users.noreply.github.com>
2025-06-10 23:35:57 -0400

    daadsffdgfdhgfsdhgfhgd

--- bot/groups/admin.py
+++ bot/groups/admin.py
@@ -460,7 +460,7 @@ class TestCog(commands.Cog):
         await interaction.response.defer(ephemeral=True)
 
         with open('logs/festivalinfobot.log', 'r', encoding='utf-8') as f:
-            lines = f.read().split('\n')[-100:]
+            lines = f.readlines()[-100:]
 
         await interaction.edit_original_response(attachments=[discord.File(io.StringIO(''.join(lines)), 'log.txt')])
 
