f6a7735fe17a0bab528d397d802b740db75024e3
tposejank <81495861+tposejank@users.noreply.github.com>
2025-10-02 06:47:27 -0400

    go fuck yourself

--- bot/tools/log.py
+++ bot/tools/log.py
@@ -91,7 +91,7 @@ def setup() -> logging.RootLogger:
     # file handler
     log_file = f"cache/logs/festivalinfobot.log"
     # Every 40mb another file will be created (?)
-    file_handler = logging.handlers.RotatingFileHandler(log_file, backupCount=3, maxBytes=40 * 1024 * 1024, encoding='utf-8')
+    file_handler = logging.handlers.RotatingFileHandler(log_file, backupCount=10, maxBytes=10 * 1024 * 1024, encoding='utf-8')
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(CustomFormatter(no_ansi=True))
 
