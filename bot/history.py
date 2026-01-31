ac575de39ef984936dc5728942741e1af7148b2e
tposejank <81495861+tposejank@users.noreply.github.com>
2026-01-30 21:38:54 -0500

    catch errors or else our bot implodes

--- bot/history.py
+++ bot/history.py
@@ -574,11 +574,29 @@ class LoopCheckHandler():
             if channel_to_send.type == 'channel':
                 channel = self.bot.get_channel(channel_to_send.id)
                 if not channel:
-                    channel = self.bot.fetch_channel(channel_to_send.id)
+                    try:
+                        channel = await self.bot.fetch_channel(channel_to_send.id)
+                    except discord.NotFound:
+                        channel = None
+                    except discord.Forbidden:
+                        logging.warning(f"Forbidden to fetch channel {channel_to_send.id}, skipping.")
+                    except discord.HTTPException as e:
+                        logging.error(f"HTTPException when fetching channel {channel_to_send.id}, skipping.", exc_info=e)
+                    except discord.InvalidData as e:
+                        logging.error(f"InvalidData when fetching channel {channel_to_send.id}, skipping.", exc_info=e)
+                    except Exception as e:
+                        logging.error(f"Unexpected error when fetching channel {channel_to_send.id}, skipping.", exc_info=e)
             elif channel_to_send.type == 'user':
                 channel = self.bot.get_user(channel_to_send.id)
                 if not channel:
-                    channel = await self.bot.fetch_user(channel_to_send.id)
+                    try:
+                        channel = await self.bot.fetch_user(channel_to_send.id)
+                    except discord.NotFound:
+                        channel = None
+                    except discord.HTTPException:
+                        logging.error(f"HTTPException when fetching user {channel_to_send.id}, skipping.", exc_info=e)
+                    except Exception as e:
+                        logging.error(f"Unexpected error when fetching user {channel_to_send.id}, skipping.", exc_info=e)
             else:
                 channel = None
 
